02f92edbb1e37710fadadecc83c530b68c154af1
Your Name <you@example.com>
2022-09-12 16:30:45 +0530

    updated

--- tally2app/urls.py
+++ tally2app/urls.py
@@ -5,13 +5,14 @@ urlpatterns = [
     path('payment',views.payment,name='payment'),
     path('contra',views.contra,name='contra'),
     path('receipt',views.receipt,name='receipt'),
-    path('purchase',views.payment,name='purchase'),
+    path('purchase',views.purchase,name='purchase'),
     path('journal',views.journal,name='journal'),
+     path('sales',views.sales,name='sales'),
     path('creditnote',views.creditnote,name='creditnote'),
-    path('debitnote',views.debitnote,name='debitnote'),
+    path('debitnote1',views.debitnote1,name='debitnote1'),
     # path('editcontra',views.editcontra,name='editcontra'),
     path('editpayment/<int:pk>',views.editpayment,name='editpayment'),
-    path('createvoucher',views.createvoucher,name='createvoucher'),
+    path('createvoucher1',views.createvoucher1,name='createvoucher1'),
     
     
 
--- tally2app/views.py
+++ tally2app/views.py
@@ -9,17 +9,19 @@ def contra(request):
     return render(request,'contra.html')
 def purchase(request):
     return render(request,'purchase.html')
+def sales(request):
+    return render(request,'sales.html')
 def receipt(request):
     return render(request,'receipt.html')
 def journal(request):
     return render(request,'journal.html')
 def creditnote(request):
     return render(request,'creditnote.html')
-def debitnote(request):
-    return render(request,'debitnote.html')
+def debitnote1(request):
+    return render(request,'debitnote1.html')
 def editcontra(request):
     return render(request,'editcontra.html')
 def editpayment(request):
     return render(request,'editpayment.html')
-def createvoucher(request):
-    return render(request,'createvoucher.html')
+def createvoucher1(request):
+    return render(request,'createvoucher1.html')
